04652dac23768cc45f73c812bf8875eda64fb965
oooooog <cigamaster@gmail.com>
2017-02-16 13:18:12 +0100

    Update quad.h

--- quad.h
+++ quad.h
@@ -2,3 +2,31 @@
 
 
 void insert(p_qtree ** node, particle p);
+
+typedef struct force{
+ double x;
+ double y;
+} force_t;
+
+typedef struct particle
+{
+   double             x_pos;
+   double             y_pos;
+   double 	     mass;
+   double      vel_x;
+   double       vel_y;
+} particle_t;
+
+struct Quadtree {
+	p_qtree * nw;
+	p_qtree * ne;
+	p_qtree * se;
+	p_qtree * sw;
+	particle p;
+	double width;
+	double centerX;
+	double centerY;
+	double massCenterX;
+	double massCenterY;
+	double mass=0;	
+}p_qtree;
