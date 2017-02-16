c61af0a567d6c7657593d66e3d1f47033a3a243a
oooooog <cigamaster@gmail.com>
2017-02-16 13:29:04 +0100

    Update quad.h

--- quad.h
+++ quad.h
@@ -1,7 +1,6 @@
 //header file for quad
 
 
-void insert(p_qtree ** node, particle p);
 
 typedef struct force{
  double x;
@@ -30,3 +29,11 @@ struct Quadtree {
 	double massCenterY;
 	double mass=0;	
 }p_qtree;
+
+void insert(p_qtree ** node, particle p);
+
+force_t getForce(p_qtree ** node, particle p, force_t force, double thetamax, double G, double epsilon);
+
+void delete(p_qtree ** node);
+
+void massification(p_qtree ** node);
