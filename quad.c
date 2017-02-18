3e4710097f2e3a07d7c5a9496f123f0afba74d3f
JoshuaHoglund <joshua.hugheshoglund@gmail.com>
2017-02-18 10:15:03 +0100

    Update quad.c

--- quad.c
+++ quad.c
@@ -133,13 +133,15 @@ void insert(p_qtree ** node, particle_t p) {
 	p_qtree * sw = (**node).sw;
 	p_qtree * ne = (**node).ne;
 	p_qtree * se = (**node).se;
+	printf("nw, sw etc exist");
 	double width = (**node).width;
 	double centerX = (**node).centerX;
 	double centerY = (**node).centerY;
 	double mass = (**node).mass;
-	
+	printf("mass, width etc exist");
 	// external node
 	if (nw==NULL) {
+		printf("NULL case");
 		nw = (p_qtree *) malloc(sizeof(p_qtree));
 		ne = (p_qtree *) malloc(sizeof(p_qtree));
 		sw = (p_qtree *) malloc(sizeof(p_qtree));
@@ -197,6 +199,7 @@ void insert(p_qtree ** node, particle_t p) {
 		}
 	}
 	else {
+		printf(" Not the NULL case");
 		int home = compass(p.x_pos, p.y_pos, centerX, centerY);
 		switch(home) {
 			case 1:
