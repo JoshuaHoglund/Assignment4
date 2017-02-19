2be251ddfeb09e61865ef97ef7511249c85fe22c
oooooog <cigamaster@gmail.com>
2017-02-19 11:55:53 +0100

    Update main.c

--- main.c
+++ main.c
@@ -222,7 +222,7 @@ int main(int argc, const char* argv[]) {
               DrawCircle(x, y, L, W, circleRadius, 0.1);          
            }
            Refresh();
-           usleep(8000);
+           usleep(80000000);
            
 
            
