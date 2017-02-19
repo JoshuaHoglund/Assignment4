a5600038f1d6c833cfc090193fdee1f80a8855f0
oooooog <cigamaster@gmail.com>
2017-02-19 12:22:01 +0100

    Update quad.c

--- quad.c
+++ quad.c
@@ -16,11 +16,12 @@ force_t* getForce(p_qtree ** node, particle_t p, force_t * force, double thetama
 	if ((**node).nw==NULL && p.x_pos!=(**node).massCenterX) {
 		double r_x = p.x_pos - (**node).massCenterX;
 		double r_y = p.y_pos - (**node).massCenterY;
-		printf("r_x: %lf \n",r_x);
-		printf("r_y: %lf \n",r_y);
+		
 		double abs_r = sqrt(r_x*r_x + r_y*r_y);
 		(*force).x = -G*p.mass*(**node).mass*r_x/((abs_r+eps)*(abs_r+eps)*(abs_r+eps));
 		(*force).y = -G*p.mass*(**node).mass*r_y/((abs_r+eps)*(abs_r+eps)*(abs_r+eps));
+		printf("force_x: %lf \n",(*force).x);
+		printf("force_y: %lf \n",(*force).y);
 		return force;
 	}
 	if (theta>thetamax && p.x_pos!=(**node).massCenterX) {
