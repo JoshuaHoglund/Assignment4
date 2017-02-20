2b65d4bf5eefddb9711e174e5833d6bdf09a7739
JoshuaHoglund <joshua.hugheshoglund@gmail.com>
2017-02-20 09:53:14 +0100

    Update quad.c

--- quad.c
+++ quad.c
@@ -13,22 +13,8 @@ double dist(double x1, double x2, double y1, double y2) {
 force_t* getForce(p_qtree ** node, particle_t p, double thetamax, double G, double eps) {
 	double theta = (**node).width/dist(p.x_pos, (**node).centerX, p.y_pos, (**node).centerY);
 	//If (**node).nw==NULL all other children are also NULL
-	if ((**node).nw==NULL && p.x_pos!=(**node).massCenterX) {
-		force_t* force = calloc(1,sizeof(force_t));
-		double r_x = p.x_pos - (**node).massCenterX;
-		double r_y = p.y_pos - (**node).massCenterY;
-		
-		double abs_r = sqrt(r_x*r_x + r_y*r_y);
-		double r3=1/((abs_r+eps)*(abs_r+eps)*(abs_r+eps));
-		(*force).x = -G*p.mass*(**node).mass*r_x*r3;
-		(*force).y = -G*p.mass*(**node).mass*r_y*r3;
-		/*if (sqrt((*force).x*(*force).x)<0.001) {
-			(*force).x = 0;
-			(*force).y = 0;
-		}*/
-		return force;
-	}
-	else if (theta>thetamax && p.x_pos!=(**node).massCenterX) {
+	
+	if (theta>thetamax && p.x_pos!=(**node).massCenterX) {
 		force_t * force = calloc(1,sizeof(force_t));
 		force_t * nwforce = getForce((&(**node).nw),p, thetamax, G, eps);
 		force_t * neforce = getForce((&(**node).ne),p, thetamax, G, eps);
@@ -44,6 +30,23 @@ force_t* getForce(p_qtree ** node, particle_t p, double thetamax, double G, doub
 		
 		return force;
 	}
+	else if ((**node).nw==NULL && p.x_pos!=(**node).massCenterX) {
+		force_t* force = calloc(1,sizeof(force_t));
+		double r_x = p.x_pos - (**node).massCenterX;
+		double r_y = p.y_pos - (**node).massCenterY;
+		
+		double abs_r = sqrt(r_x*r_x + r_y*r_y);
+		double r3=1/((abs_r+eps)*(abs_r+eps)*(abs_r+eps));
+		(*force).x = -G*p.mass*(**node).mass*r_x*r3;
+		(*force).y = -G*p.mass*(**node).mass*r_y*r3;
+		/*if (sqrt((*force).x*(*force).x)<0.001) {
+			(*force).x = 0;
+			(*force).y = 0;
+		}*/
+		return force;
+	}
+	
+	
 	else if(p.x_pos!=(**node).massCenterX) {
 		force_t * force = calloc(1,sizeof(force));
 		double r_x = p.x_pos - (**node).massCenterX;
